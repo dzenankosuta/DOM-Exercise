50b9099a35ead4654d605365d1d380c139b74e6f
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-10-02 18:05:22 +0200

    Finishing first exercise

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-// EXAMINE THE DOCUMENT OBJECT
+// EXAMINE THE DOCUMENT OBJECT //
 // console.dir(document);
 
 console.log(document.domain);
@@ -17,10 +17,10 @@ console.log(document.forms);
 console.log(document.links);
 console.log(document.images);
 
-// GETELEMENTBYID
+// GETELEMENTBYID //
 
 // console.log(document.getElementById("header-title"));
-let headerTitle = document.getElementById("header-title");
+let headerTitle = document.getElementById("header-title"); // pristupanje elementu sa datim id
 let header = document.getElementById("main-header");
 console.log(headerTitle);
 // headerTitle.textContent = "Hello";
@@ -29,5 +29,71 @@ console.log(headerTitle.innerText); // slusa stilove
 
 headerTitle.innerHTML = "<h3>Hello</h3>";
 
-// PPROMENA STILOVA
+// PPROMENA STILOVA //
 header.style.borderBottom = "solid 3px #000";
+
+// GETELEMENTSBYCLASSNAME //
+let items = document.getElementsByClassName("list-group-item"); // uzimanje svih elemenata sa datom klasom
+console.log(items);
+console.log(items[1]); // pristupanje drugom takvom elementu
+items[1].textContent = "Hello 2"; // promena sadrzaja
+items[1].style.fontWeight = "bold"; // promena stilova..
+items[1].style.backgroundColor = "yellow";
+
+// items.style.backgroundColor = "#f4f4f4"; nece da radi. Vec mora preko for petlje:
+
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "#f4f4f4";
+// }
+
+// GETELEMENTSBYTAGNAME //
+
+let li = document.getElementsByTagName("li"); // uzimanje svih elemenata sa datom klasom
+console.log(li);
+console.log(li[1]); // pristupanje drugom takvom elementu
+li[1].textContent = "Hello 2"; // promena sadrzaja
+li[1].style.fontWeight = "bold"; // promena stilova..
+li[1].style.backgroundColor = "yellow";
+
+// li.style.backgroundColor = "#f4f4f4"; nece da radi. Vec mora preko for petlje:
+
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.backgroundColor = "#f4f4f4"; // primenjuje se na sve li tagove, bez obzira na klasu
+// }
+
+// QUERYSELECTOR //
+
+// po defaultu uzima samo prvi takav selector, querySelectorAll nam daje sve.
+
+let header2 = document.querySelector("#main-header"); // uzima prvi element sa datim id
+header2.style.borderBottom = "solid 4px #ccc";
+
+let input = document.querySelector("input"); // uzima prvi element sa datim HTML tagom
+input.value = "Hello World";
+
+let submit = document.querySelector("input[type='submit']"); // uzima dati element  sa odredjenim atributom
+submit.value = "SEND";
+
+let items2 = document.querySelector(".list-group-item");
+items2.style.color = "red";
+
+let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+secondItem.style.color = "blue";
+
+// let lastItem = document.querySelector(".list-group-item:last-child");
+// lastItem.style.color = "blue";
+
+// QUERYSELECTORALL //
+
+let titles = document.querySelectorAll(".title");
+
+console.log(titles);
+titles[0].textContent = "Hello";
+
+let odd = document.querySelectorAll("li:nth-child(odd)");
+let even = document.querySelectorAll("li:nth-child(even)");
+
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "#f4f4f4";
+  even[i].style.backgroundColor = "#ccc";
+}
